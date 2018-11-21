a83fc6aae7c1c2aae6ee71699a59d0f004d6ca29
vapa1996 <saravapa1996@gmail.com>
2018-11-21 10:38:04 +0100

    correcció exercici bbdd vols

--- itacademy_USFlights_[SaraVarasPaneque].sql
+++ itacademy_USFlights_[SaraVarasPaneque].sql
@@ -6,25 +6,25 @@ SELECT Origin, AVG(ArrDelay), AVG(DepDelay) FROM usairlineflights2.flights GROUP
 
 /* Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen. */
 SELECT Origin, colYear, colMonth,  if (AVG(ArrDelay)>=1, 'Retard','En hora')  
-FROM usairlineflights2.flights  WHERE ArrDelay >=1
+FROM usairlineflights2.flights  /*WHERE ArrDelay >=1*/
 GROUP BY Origin, colYear, colMonth ORDER BY Origin, colYear, colMonth;
 
 /*Retard promig d’arribada dels vols, per mesos i segons l’aeroport origen 
 canviant el codi de l'aeroport pel nom de la ciutat*/
 SELECT City, colYear, colMonth, if (AVG(ArrDelay)>=1,'Retard', 'En hora') 
 FROM usairlineflights2.flights 
-INNER JOIN usairlineflights2.usairports on usairlineflights2.flights.Origin = 
+LEFT JOIN usairlineflights2.usairports on usairlineflights2.flights.Origin = 
 usairlineflights2.usairports.IATA WHERE ArrDelay >=1
 GROUP BY City, colYear, colMonth ORDER BY City, colYear, colMonth;
 
 /*Companyies amb més vols cancelats*/
-SELECT Description, SUM(Cancelled) FROM usairlineflights2.flights
+SELECT Description, COUNT(Cancelled) FROM usairlineflights2.flights
 INNER JOIN 	usairlineflights2.carriers on usairlineflights2.flights.UniqueCarrier = 
 usairlineflights2.carriers.CarrierCode WHERE Cancelled =1 GROUP BY Description
 ORDER BY SUM(Cancelled) DESC;
 
 /*L’identificador dels 10 avions que més distància han recorregut fent vols.*/
-SELECT TailNum, SUM(Distance) FROM usairlineflights2.flights GROUP BY TailNum 
+SELECT TailNum, SUM(Distance) FROM usairlineflights2.flights WHERE TailNum !='NA' GROUP BY TailNum 
 ORDER BY SUM(Distance) DESC LIMIT 10;
 
 /*Companyies amb el seu retard promig només d’aquelles les quals els seus vols 
@@ -32,4 +32,4 @@ arriben al seu destí amb un retràs promig major de 10 minuts.*/
 SELECT Description, AVG(ArrDelay)
 FROM usairlineflights2.flights LEFT JOIN usairlineflights2.carriers on usairlineflights2.flights.UniqueCarrier =
 usairlineflights2.carriers.CarrierCode WHERE ArrDelay > 10
-GROUP BY Description ORDER BY AVG(ArrDelay);
+GROUP BY Description ORDER BY AVG(ArrDelay) ASC;
